30d5b893e2b6d10d5bbabf86498d460954e95d9f
Alexey <fisenko_alex@mail.ru>
2020-11-15 17:37:07 +0300

    heroku and docker support

--- .travis/deploy_heroku.sh
+++ .travis/deploy_heroku.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 curl https://cli-assets.heroku.com/install-ubuntu.sh | sh
-heroku plugins:install heroku-container-registry
-docker login -u $DOCKER_USER -p $DOCKER_PASS --password=$HEROKU_PASSWORD registry.heroku.com
+heroku container:login
 heroku container:push web --app $HEROKU_APP_NAME
+heroku container:release web --app $HEROKU_APP_NAME
