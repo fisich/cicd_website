d59477f62e2c94e34d4fb699dcad48afa80de9a0
Alexey <fisenko_alex@mail.ru>
2020-11-15 18:39:41 +0300

    HEROKU

--- .travis/deploy_heroku.sh
+++ .travis/deploy_heroku.sh
@@ -1,6 +1,5 @@
 #!/bin/sh
 curl https://cli-assets.heroku.com/install-ubuntu.sh | sh
-heroku plugins:install heroku-container-registry
-docker login -e _ -u _ --password=$HEROKU_API_KEY registry.heroku.com
+docker login --username=$HEROKU_USER --password=$HEROKU_API_KEY registry.heroku.com
 heroku container:push web --app $HEROKU_APP_NAME
 heroku container:release web --app $HEROKU_APP_NAME
