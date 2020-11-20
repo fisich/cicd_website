d18114d6b84a81152c1a960eb153e33f933ab1aa
Alexey <fisenko_alex@mail.ru>
2020-11-20 19:16:32 +0300

    корректировка переменных

--- tests/test_parser.py
+++ tests/test_parser.py
@@ -4,18 +4,20 @@ from project import parser
 
 
 def test_put_any_website():
-    l = ('https://www.google.com', 'https://yandex.ru/news/?utm_source=main_stripe_big', 'https://mignews.com/')
-    for url in l[:2]:
+    incorrect = ('https://www.google.com', 'https://yandex.ru/news/?utm_source=main_stripe_big')
+    correct = 'https://mignews.com/'
+    for url in incorrect:
         news = parser.collectNews(url)
         assert len(news) == 0
-    news = parser.collectNews(l[2])
+    news = parser.collectNews(correct)
     assert len(news) > 0
 
 
 def test_generate_table_on_wrong_input():
-    l = ('1', 3, (3, 5, 76, 8), parser.collectNews('https://mignews.com/'))
-    for val in l[:3]:
+    incorrect = ('1', 3, (3, 5, 76, 8))
+    correct = parser.collectNews('https://mignews.com/')
+    for val in incorrect:
         htmlcode = parser.generate_table(val)
         assert htmlcode == ""
-    htmlcode = parser.generate_table(l[3])
+    htmlcode = parser.generate_table(correct)
     assert len(htmlcode) > 0
